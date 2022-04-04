3429ac623fd2790b5560455753268d436adcea44
Viktar Prakopchyk <v.prakopchyk@andersenlab.com>
2022-04-05 00:26:41 +0300

    add some API tests

---
+++ src/test/java/api/UnSeccessReg.java
@@ -0,0 +1,13 @@
+package api;
+
+public class UnSeccessReg {
+    private String error;
+
+    public UnSeccessReg(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
